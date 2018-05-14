95ba869e4045d267062c3c01091c121141f7004d
Jesse McCormick <jesse.mccormick@scipax.com>
2018-05-14 11:32:03 -0400

    Allow the image alignment styles applied by the toolbar to be saved/processed by Quill.

--- src/ImageResize.js
+++ src/ImageResize.js
@@ -203,10 +203,45 @@ export default class ImageResize {
         }
     };
 }
-
+		
 if (window.Quill) {
 	
-  //Add support for IE 11
+	//BEGIN allow image alignment styles
+	const ImageFormatAttributesList = [
+		'alt',
+		'height',
+		'width',
+		'style'
+	];
+		
+	var BaseImageFormat = window.Quill.import('formats/image');
+	class ImageFormat extends BaseImageFormat {
+	  static formats(domNode) {
+		return ImageFormatAttributesList.reduce(function(formats, attribute) {
+		  if (domNode.hasAttribute(attribute)) {
+			formats[attribute] = domNode.getAttribute(attribute);
+		  }
+		  return formats;
+		}, {});
+	  }
+	  format(name, value) {
+		if (ImageFormatAttributesList.indexOf(name) > -1) {
+		  if (value) {
+			this.domNode.setAttribute(name, value);
+		  } else {
+			this.domNode.removeAttribute(name);
+		  }
+		} else {
+		  super.format(name, value);
+		}
+	  }
+	}
+
+	window.Quill.register(ImageFormat, true);
+	//END allow image alignment styles
+
+
+	//Add support for IE 11
 	if (typeof Object.assign != 'function') {
 	  Object.assign = function(target) {
 	    'use strict';
